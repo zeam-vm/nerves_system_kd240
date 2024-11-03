16e38d236e598cb708f98c468327b9923b2ea744
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-11-03 16:27:37 +0900

    Fix uboot/boot.cmd, remove hard code
    USB ストレージを挿入した上で `Nerves.Runtime.revert` をした場合、usb 0 が USB ストレージになって
    revert に失敗する。それを修正した。

--- uboot/boot.cmd
+++ uboot/boot.cmd
@@ -1,6 +1,10 @@
 setenv bootargs "root=/dev/sda2 rootwait earlycon console=ttyPS1,115200 clk_ignore_unused uio_pdrv_genirq.of_id=generic-uio xilinx_tsn_ep.st_pcp=4 cma=512M"
 
-load usb 0:1 0x00200000 /Image
-load usb 0:1 0x44000000 /zynqmp-smk-k24-revA-sck-kd-g-revA.dtb
-
-booti 0x00200000 - 0x44000000
+if test "${devtype}" = "usb"; then
+  if test -e ${devtype} ${devnum}:${distro_bootpart} /Image && \
+     test -e ${devtype} ${devnum}:${distro_bootpart} /zynqmp-smk-k24-revA-sck-kd-g-revA.dtb; then
+    load ${devtype} ${devnum}:${distro_bootpart} 0x00200000 /Image
+    load ${devtype} ${devnum}:${distro_bootpart} 0x44000000 /zynqmp-smk-k24-revA-sck-kd-g-revA.dtb
+    booti 0x00200000 - 0x44000000
+  fi
+fi
